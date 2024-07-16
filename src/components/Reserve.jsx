9533c1e707ed716d45d8017823fc37f94e5e1e62
jwt2706 <jwt2home@gmail.com>
2024-07-16 14:10:33 -0400

    fix image height problem

--- src/components/Reserve.jsx
+++ src/components/Reserve.jsx
@@ -24,21 +24,19 @@ function Reserve() {
   };
 
   const isFormValid = name && email && seatings && startDate;
-
+  
   return (
     <div className="max-w-screen-4xl mx-auto">
       <div className="container mx-auto mt-5 p-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="w-full">
-            <div className="overflow-hidden rounded-lg shadow-md">
-              <img
-                src={ReserveImg}
-                alt="Contact image"
-                className="w-full h-full object-cover"
-              />
-            </div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full md:w-1/2">
+            <img
+              src={ReserveImg}
+              alt="Contact image"
+              className="overflow-hidden rounded-lg shadow-md w-full h-full object-cover"
+            />
           </div>
-          <div className="px-3 mb-6 md:mb-0">
+          <div className="w-full md:w-1/2 flex flex-col justify-center">
             <h3 className="mb-2">{t("BookAReservation")}</h3>
             <h4 className="text-lg mb-4">{t("ReservationInstructions")}</h4>
             <form onSubmit={handleSubmit}>
@@ -76,52 +74,36 @@ function Reserve() {
                   required
                 />
               </div>
-              <div className="flex justify-between gap-4 mb-6">
-                <div className="flex-1">
-                  <label
-                    htmlFor="date-time"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    {t("DateAndTime")}
-                  </label>
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    showTimeSelect
-                    dateFormat="Pp"
-                    className="mt-1 p-1 block w-full rounded-lg border border-gray-300 focus:border-black"
-                    required
-                  />
-                </div>
-                <div className="flex-1">
-                  <label
-                    htmlFor="seatings"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    {t("NumberOfSeatings")}
-                  </label>
-                  <input
-                    type="number"
-                    className="mt-1 p-1 block w-full rounded-lg border border-gray-300 focus:border-black"
-                    id="seatings"
-                    placeholder="4"
-                    value={seatings}
-                    onChange={(e) => {
-                      let value = e.target.value;
-                      // make sure the input makes sense, we dont want 0.5 persons or -4 persons lol
-                      value = value.replace(/[-.]/g, "");
-                      if (
-                        value === "" ||
-                        (/^\d+$/.test(value) && parseInt(value, 10) > 0)
-                      ) {
-                        setSeatings(value);
-                      } else {
-                        setSeatings("");
-                      }
-                    }}
-                    required
-                  />
-                </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="seatings"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {t("Seatings")}
+                </label>
+                <input
+                  type="number"
+                  className="mt-1 p-1 block w-full rounded-lg border border-gray-300 focus:border-black"
+                  id="seatings"
+                  placeholder="2"
+                  value={seatings}
+                  onChange={(e) => setSeatings(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="startDate"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {t("Date")}
+                </label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  className="mt-1 p-1 block w-full rounded-lg border border-gray-300 focus:border-black"
+                  required
+                />
               </div>
               <button
                 type="submit"
